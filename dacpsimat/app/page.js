62e6956f827e75541f4a35694cbcb46205daa687
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-05-10 09:07:50 +0100

    a

--- dacpsimat/app/page.js
+++ dacpsimat/app/page.js
@@ -3,8 +3,13 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+     <div className="min-h-full min-w-full flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8 ">
+      <h1 className="text-6xl font-bold text-center text-gray-800">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
 
+
+      </div>
     </main>
   )
 }
